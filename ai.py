6bd7e77cfb820905a1d4529173359e5463e953bf
MrRifat555 <alexrifatmahmud@gmail.com>
2026-07-11 20:20:14 +0600

    Implement AI prompt handling in ai.py

---
+++ ai.py
@@ -0,0 +1,13 @@
+from config import client
+
+MODEL_NAME = "gemini-2.5-flash"
+
+
+def ask_ai(prompt):
+
+    response = client.models.generate_content(
+        model=MODEL_NAME,
+        contents=prompt
+    )
+
+    return response.text
