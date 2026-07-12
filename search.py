2937c68ecf49f5845a43a24bf44f793a03cca7d3
MrRifat555 <alexrifatmahmud@gmail.com>
2026-07-12 07:22:29 +0600

    Implement AI search function using Google GenAI

---
+++ search.py
@@ -0,0 +1,20 @@
+from config import client
+from google.genai import types
+
+MODEL_NAME = "gemini-2.5-flash"
+
+def search_ai(prompt):
+
+    response = client.models.generate_content(
+        model=MODEL_NAME,
+        contents=prompt,
+        config=types.GenerateContentConfig(
+            tools=[
+                types.Tool(
+                    google_search=types.GoogleSearch()
+                )
+            ]
+        )
+    )
+
+    return response.text
