12b938122f6230be0d7fd0dd3f56dbd8143d8252
MrRifat555 <alexrifatmahmud@gmail.com>
2026-07-15 13:45:21 +0600

    Add Supabase client initialization to config.py

--- config.py
+++ config.py
@@ -1,8 +1,19 @@
 import os
 from google import genai
+from supabase import create_client
 
+# Gemini
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 client = genai.Client(
     api_key=GEMINI_API_KEY
+)
+
+# Supabase
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
 )
